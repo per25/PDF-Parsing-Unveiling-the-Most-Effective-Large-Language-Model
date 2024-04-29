37f1217616be007988817874754a000ebf30a0f0
Per Ingemarsson <per.259@hotmail.com>
2024-04-29 16:52:03 +0200

    Update main.py to include question_handler module

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pdf_parser
+import question_handler
 
-INPUT_FOLDER_PATH = "input_data/pdf"
+INPUT_PDF_FOLDER_PATH = "input_data/pdf"
+INPUT_QUESTIONS_FOLDER_PATH = "input_data/questions"
 OUTPUT_FOLDER_PATH = "output_data"
 
 
 def main():
-    pdf_parser.run_all(INPUT_FOLDER_PATH, OUTPUT_FOLDER_PATH)
+    pdf_parser.run_all(INPUT_PDF_FOLDER_PATH, OUTPUT_FOLDER_PATH)
+    question_handler.run(OUTPUT_FOLDER_PATH, INPUT_QUESTIONS_FOLDER_PATH)
 
 
 if __name__ == "__main__":
